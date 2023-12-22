709c3ab3f7a30e314ee73a0dd4218bf508ed396e
Prasanna <prasannakumar.akrabhi@gmail.com>
2023-12-22 21:43:09 +0530

    fix bug

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,9 +3,11 @@ const router = Express.Router();
 import UserController from "../controller/userController.js";
 import checkUserAuth from "../middelware/auth.js";
 
-router.post("/login", checkUserAuth);
+router.post("/changepassword", checkUserAuth);
 
 router.post("/register", UserController.userRegistretion);
 router.post("/login", UserController.userLogin);
+
+router.post("/changepassword", UserController.changeUserPassword);
 
 export default router;
